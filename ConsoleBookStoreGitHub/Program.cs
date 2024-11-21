d5d3df919be607d64e4e6e2ff36cafdf3daef0bc
uchqunov <uchqunov@outlook.com>
2024-11-21 18:28:06 +0500

    New little changes from develop to main testes!

--- ConsoleBookStoreGitHub/Program.cs
+++ ConsoleBookStoreGitHub/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Yangi o'zgarishlar!");
+            Console.WriteLine("Develop branchidan ovommiz!");
         }
     }
 }
