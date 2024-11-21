1287351814057a2668ac1034e464ec3427a6860b
uchqunov <uchqunov@outlook.com>
2024-11-21 17:26:33 +0500

    BookStore Alpha

---
+++ ConsoleBookStoreGitHub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleBookStoreGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salom bu kitob magazin!");
+        }
+    }
+}
